1a7bfdcf958bc9d0a59832507d7d9eba87dc95bf
gmsanjuliano <gmsanjuliano@gmail.com>
2022-03-02 22:00:23 -0300

    innitial-commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace C_
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      decimal km, lts, media = 5.22m;
+
+      Console.WriteLine("Informa a distância percorrida: ");
+      decimal.TryParse(Console.ReadLine(), out km);
+
+      Console.WriteLine("Informe a quantidade de gasolina gasta: ");
+      decimal.TryParse(Console.ReadLine(), out lts);
+      media = km / lts;
+
+      Console.WriteLine($"Média de litros por km: {media.ToString("0.##")}");
+
+
+      Console.ReadLine();
+    }
+  }
+}
